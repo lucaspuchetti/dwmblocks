079bd0c59b11f45153b8028e3e0afc8e74dca938
lucaspuchetti <lucaspuchetti@gmail.com>
2021-06-21 23:51:01 +0200

    Agrego nivel de brillo del monitor principal

--- config.h
+++ config.h
@@ -15,12 +15,13 @@ static const Block blocks[] = {
 	/* {"",	"sb-price btc Bitcoin 💰",				9000,	21}, */
 	//{"",	"sb-torrent",	20,	7},
 	{"",	"sb-memory",	10,	14}, 
-	{"",	"sb-cpu",		10,	18}, 
+	{"",	"sb-cpu",	10,	18}, 
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	//{"",	"sb-forecast",	18000,	5},
 	//{"",	"sb-mailbox",	180,	12},
 	{"",	"sb-nettraf",	1,	16},
 	{"",	"sb-volume",	0,	10},
+	{"",	"sb-brightness",0,	12},
 	{"",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
 	{"",	"sb-internet",	5,	4},
